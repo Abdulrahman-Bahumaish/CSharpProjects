c0cd3ec46a548b66ad1bd25a0c7d27acb2796c51
Abdulrahman-Bahumaish <abdulrahmanakram77@gmail.com>
2023-05-18 22:27:49 +0300

    This Is Matrix Exercise

---
+++ ExercisesOOPMatrix/ExercisesOOPMatrix/Matrix.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Write a class named Matrix that applies the following methods on a 2D-Array:
+//-ReadMatrix: reads values from the user and assigns them to the 2D-Array.
+//-Transpose: transpose the matrix.
+//- Print: prints the 2D-Array as a matrix
+namespace ExercisesOOPMatrix
+{
+    internal class Matrix
+    {
+
+        private int[,] Matrixs;
+        private int Columns;
+        private int Rows;
+
+        public Matrix(int Columns, int Rows)
+        {
+            this.Columns = Columns;
+            this.Rows = Rows;
+            this.Matrixs = new int[Columns, Rows];
+        }
+        public void ReadMatrix()
+        {
+            for (int C = 0; C < Columns; C++)
+            {
+                for (int R = 0; R < Rows; R++)
+                {
+                    Console.WriteLine("Matrixs[{0},{1}]:", C, R);
+
+                    //int value =int.Parse(Console.ReadLine());
+                    //this.Matrixs[C, R] = value;
+                }
+            }
+        }
+
+        public void TransposeMatrix()
+        {
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("---------------------");
+            Console.WriteLine("TransposeMatrix :");
+            for (int C = 0; C < Rows; C++)
+            {
+                for (int R = 0; R < Columns; R++)
+                {
+                    Console.WriteLine("Matrixs[{1},{0}]:", C, R);
+                }
+            }
+        }
+       
+    }
+}
---
+++ ExercisesOOPMatrix/ExercisesOOPMatrix/Program.cs
@@ -0,0 +1,8 @@
+﻿using ExercisesOOPMatrix;
+
+Console.WriteLine("Enter The Two Numbers :");
+int C = Convert.ToInt32(Console.ReadLine());
+int R = Convert.ToInt32(Console.ReadLine());
+Matrix MatrixTowDimn = new Matrix(C, R);
+MatrixTowDimn.ReadMatrix();
+MatrixTowDimn.TransposeMatrix();
